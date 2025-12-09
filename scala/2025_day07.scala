e2db69a0b2dde99a5dca54b5f621056ceefdb43e
Jamie Thompson <bishbashboshjt@gmail.com>
2025-12-09 07:25:15 +0100

    cleanups

--- scala/2025_day07.scala
+++ scala/2025_day07.scala
@@ -4,8 +4,8 @@ import challenges.*
 import scala.util.chaining.*
 import scala.language.experimental.relaxedLambdaSyntax
 
-@main def part1 = println(solve1(inputToday("")))
-@main def part2 = println(solve2(inputToday("")))
+@main def part1 = println(solve1(inputToday()))
+@main def part2 = println(solve2(inputToday()))
 
 def solve1(input: String): Int =
   val grid = input.linesIterator.map(_.toCharArray.pipe(IArray.from(_))).pipe(IArray.from(_))
@@ -28,29 +28,26 @@ def solve1(input: String): Int =
 def solve2(input: String): Long =
   val grid = input.linesIterator.map(_.toCharArray.pipe(IArray.from(_))).pipe(IArray.from(_))
   val valid = 0 to grid.size
-  val state = (paths = Map.empty[Int, Long])
-  val state0 = grid.zipWithIndex.foldLeft(state): (state, rowI) =>
-    val (row, i) = rowI
-    if state.paths.isEmpty then
+  val paths0 = grid.foldLeft(Map.empty[Int, Long]): (paths, row) =>
+    if paths.isEmpty then
       // first row
-      (paths = Map(row.indexOf('S') -> 1L))
+      Map(row.indexOf('S') -> 1L)
     else
-      val candidates = IArray.from(state.paths.keySet.filter(row(_) == '^'))
+      val candidates = IArray.from(paths.keySet.filter(row(_) == '^'))
       if candidates.isEmpty then
-        state
+        paths
       else
         val projection = candidates
           .map: pos => pos -> IArray(pos - 1, pos + 1).filter(valid.contains(_))
           .toMap
 
         val destinations = IArray.from(projection.values.flatten).distinct
-        val olds = destinations.map: col => col -> state.paths.getOrElse(col, 0L)
+        val olds = destinations.flatMap: col => paths.get(col).map(count => col -> count)
         val nexts = candidates.flatMap: col =>
-          val old = state.paths(col)
-          projection(col).map(_ -> old)
+          val count = paths(col)
+          projection(col).map(_ -> count)
         val combined = (olds ++ nexts)
           .groupMapReduce((col, _) => col)((_, count) => count)(_ + _)
 
-        val paths0 = state.paths -- candidates ++ combined
-        (paths = paths0)
-  state0.paths.values.sum
+        paths -- candidates ++ combined
+  paths0.values.sum
